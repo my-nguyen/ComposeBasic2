dbb91959ff4d947d0dd2060f60e106b741eb46ef
Mỹ Nguyễn <nguyen@Mys-Laptop.local>
2023-06-13 15:37:28 -0700

    b3: Reusing composables

--- app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
+++ app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
@@ -19,15 +19,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasics2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
+        Greeting("Android")
     }
 }
 
