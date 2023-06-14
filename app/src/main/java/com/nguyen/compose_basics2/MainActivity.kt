165025eeb6b4ceca6aefc7532eab98b32e894d7f
Mỹ Nguyễn <nguyen@Mys-Laptop.local>
2023-06-13 22:27:20 -0700

    b11: Finishing touches

--- app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
+++ app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
@@ -4,17 +4,22 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,47 +65,14 @@ private fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Greeting(name: String) {
-    // To add internal state to a composable, use the mutableStateOf function. State and
-    // MutableState are interfaces that hold some value and trigger UI updates (recompositions)
-    // whenever that value changes
-    // To preserve state across recompositions, remember the mutable state using 'remember'
-    var expanded by remember { mutableStateOf(false) }
-    // animateDpAsState composable takes a targetValue whose type is Dp
-    // extraPadding depends on the expanded state
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        // animationSpec lets you customize the animation
-        // spring relies on physical properties (damping and stiffness) to make animations more natural
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow)
-    )
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+    Card(
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            // to place a composable (ElevatedButton) at the end of a Row: give some weight to the
-            // composable at the start (Column)
-            Column(modifier = Modifier
-                .weight(1f)
-                // expand an item when requested
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                // use font 'headlineMedium' and style 'ExtraBold'
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
-                    fontWeight = FontWeight.ExtraBold
-                ))
-            }
-            // Button is a composable provided by the material3 package which takes a composable as
-            // the last argument.
-            // toggle the value of the expanded state and show a different text depending on the value.
-            ElevatedButton(onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show less" else "Show more")
-            }
-        }
+        CardContent(name)
     }
 }
 
@@ -134,5 +106,41 @@ fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifie
 fun OnboardingPreview() {
     ComposeBasics2Theme {
         OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier
+        .padding(12.dp)
+        // automate the process of creating the animation
+        .animateContentSize(animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow)
+        )
+    ) {
+        Column(modifier = Modifier.weight(1f).padding(12.dp)) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            // Text is displayed when the item is expanded
+            if (expanded) {
+                Text(text = ("Composem ipsum color sit lazy, padding theme elit, sed do bouncy. ").repeat(4),)
+            }
+        }
+        // Use the IconButton composable together with a child Icon
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                // Use Icons.Filled.ExpandLess and Icons.Filled.ExpandMore, which are available in
+                // the material-icons-extended artifact
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                // for accessibility
+                contentDescription = stringResource(id = if (expanded) R.string.show_less else R.string.show_more)
+            )
+        }
     }
 }
