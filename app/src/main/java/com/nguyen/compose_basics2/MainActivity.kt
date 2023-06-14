3a0cf3b6373b29e0872a69f22b9f780cb8636300
Mỹ Nguyễn <nguyen@Mys-Laptop.local>
2023-06-13 18:22:37 -0700

    b7: Creating a performant lazy list

--- app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
+++ app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -40,10 +42,12 @@ fun MyApp(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun Greetings(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+// names defaults to a list of 1000 strings, filled with the values contained in its lambda
+private fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }) {
+    // LazyColumn and LazyRow are equivalent to RecyclerView in Android Views
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { index ->
+            Greeting(name = index)
         }
     }
 }
