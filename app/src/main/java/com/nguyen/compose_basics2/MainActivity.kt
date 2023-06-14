3789ccbea63b4834886c5b7d6623b1a47ab48ead
Mỹ Nguyễn <nguyen@Mys-Laptop.local>
2023-06-13 22:00:38 -0700

    b10: Styling and theming your app

--- app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
+++ app/src/main/java/com/nguyen/compose_basics2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nguyen.compose_basics2
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nguyen.compose_basics2.ui.theme.ComposeBasics2Theme
@@ -87,7 +89,10 @@ private fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                // use font 'headlineMedium' and style 'ExtraBold'
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             // Button is a composable provided by the material3 package which takes a composable as
             // the last argument.
@@ -99,6 +104,7 @@ private fun Greeting(name: String) {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "DarkWing")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -128,21 +134,5 @@ fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifie
 fun OnboardingPreview() {
     ComposeBasics2Theme {
         OnboardingScreen(onContinueClicked = {})
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-private fun GreetingsPreview() {
-    ComposeBasics2Theme {
-        Greetings()
-    }
-}
-
-@Preview
-@Composable
-fun MyAppPreview() {
-    ComposeBasics2Theme {
-        MyApp(Modifier.fillMaxSize())
     }
 }
